1570512d6c306ecf502f6106e2970158476e1ab5
helitonns <helitonns@gmail.com>
2025-08-29 20:30:42 -0400

    Crie o componente InputText

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import PlusIcon from "./assets/icons/plus.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
 import ButtonIcon from "./components/button-icon";
 import Button from "./components/buttton";
+import InputText from "./components/input-text";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-8 bg-gray-100">
       <Button icon={SpinnerIcon} >Salvar</Button>
 
       <div className="flex gap-1">
@@ -15,6 +16,10 @@ function App() {
         <ButtonIcon icon={PencilIcon} variant={"secondary"}/>
         <ButtonIcon icon={PlusIcon}  variant={"tertiary"}/>
         <ButtonIcon icon={SpinnerIcon} />
+      </div>
+
+      <div>
+        <InputText />
       </div>
     </div>
   );
