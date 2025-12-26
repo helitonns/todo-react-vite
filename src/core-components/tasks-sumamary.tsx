0748feda780b989d97f06dbd770a3ec8e87c8686
helitonns <helitonns@gmail.com>
2025-12-26 10:27:59 -0400

    Atualize as Badges de Resumo das Tarefas

--- src/core-components/tasks-sumamary.tsx
+++ src/core-components/tasks-sumamary.tsx
@@ -1,17 +1,20 @@
 import Badge from "../components/badge";
 import Text from "../components/text";
+import useTasks from "../hooks/use-tasks";
 
 
 export default function TasksSummary() {
+  const {tasksCount, concludedTasksCount} = useTasks();
+
   return(
     <>
       <div className="flex items-center gap-2">
         <Text variant="body-sm-bold" className="!text-gray-300">Tarefas Criadas</Text>
-        <Badge variant="secondary">5</Badge>
+        <Badge variant="secondary">{tasksCount}</Badge>
       </div>
       <div className="flex items-center gap-2">
         <Text variant="body-sm-bold" className="!text-gray-300">Concluidas</Text>
-        <Badge variant="primary">2 de 5</Badge>
+        <Badge variant="primary">{concludedTasksCount} de {tasksCount}</Badge>
       </div>
     </>
   );
